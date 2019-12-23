407e74e96aae8d93341f4af5af744cc292aaf3a5
namhoangle <happybear02042000@gmail.com>
2019-12-23 17:52:26 +0700

    check database token_devices to send message notification

--- go.mod
+++ go.mod
@@ -5,10 +5,20 @@ go 1.13
 require (
 	cloud.google.com/go/firestore v1.1.0 // indirect
 	cloud.google.com/go/storage v1.4.0 // indirect
-	firebase.google.com/go v3.10.0+incompatible // indirect
-	github.com/labstack/echo v3.3.10+incompatible // indirect
+	firebase.google.com/go v3.10.0+incompatible
+	github.com/google/go-cmp v0.3.1 // indirect
+	github.com/hashicorp/golang-lru v0.5.3 // indirect
+	github.com/jinzhu/gorm v1.9.11
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/lib/pq v1.2.0 // indirect
+	github.com/mhewedy/echox v0.0.0-20190817021110-e5d1959db347
+	github.com/qor/audited v0.0.0-20171228121055-b52c9c2f0571
+	github.com/qor/qor v0.0.0-20191022064424-b3deff729f68 // indirect
 	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413 // indirect
+	golang.org/x/net v0.0.0-20191109021931-daa7c04131f5 // indirect
 	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6 // indirect
+	golang.org/x/sys v0.0.0-20191110163157-d32e6e3b99c4 // indirect
 	google.golang.org/appengine v1.6.5 // indirect
+	google.golang.org/grpc v1.25.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
 	"bytes"
 	"context"
 	"encoding/json"
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/messaging"
 	"github.com/labstack/echo"
 	"io/ioutil"
 	"log"
 	"net/http"
-	firebase "firebase.google.com/go"
-	"firebase.google.com/go/messaging"
-
 )
 
 var Users = make(map[string]*User)
 var Users2 = make([]*User, 0)
 
 type (
+	HomegateUsers struct {
+		Id         int    `json:"id"`
+		UserId     string `json:"user_id"`
+		HomegateId string `json:"homegate_id"`
+	}
+	TokenDevices struct {
+		TokenDevice string `json:"token_devices"
+`
+	}
+
 	User struct {
-		Email string `json:"email" xml:"email" form:"email" query:"email"`
+		Email  string `json:"email" xml:"email" form:"email" query:"email"`
 		Tokens []string
 	}
 	Device struct {
@@ -26,104 +39,202 @@ type (
 		Token string `json:"token"`
 	}
 	Data struct {
-		 Feature string `json:"feature"`
-		 Body string `json:"body"`
+		Title        string `json:"title"`
+		Body         string `json:"body"`
+		Notification messaging.Notification
+	}
+	Notification struct {
+		Title string `json:"title"`
+		Body  string `json:"body"`
 	}
 
 	Payload struct {
-		RegistrationIds []string `json:"registration_ids"`
-		Data Data `json:"data"`
+		RegistrationIds []string     `json:"registration_ids"`
+		Data            Data         `json:"data"`
+		Notification    Notification `json:"notification"`
 	}
 )
 
-func main()  {
+func main() {
+	//username := os.Getenv("db_user")
+	//password := os.Getenv("db_pass")
+	//dbName := os.Getenv("db_name")
+	//dbHost := os.Getenv("db_host")
+	//log.Print("valueeeeLL",dbHost,username,password)
+	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", "localhost", "postgres", "auth", "postgres") //Build connection string
+	dbCon, err := gorm.Open("postgres", dbUri)
+	if err != nil {
+		panic(err)
+	}
+	defer dbCon.Close()
+
+	database := dbCon.DB()
+	err = database.Ping()
+	if err != nil {
+		panic(err)
+	}
+	log.Print("connected to DB")
+
 	e := echo.New()
+
 	e.POST("/user/register", RegisterUser)
 
 	http.HandleFunc("device/register", RegisterDevice)
-    e.POST("/device/push", Push)
+	e.POST("/device/push", Push(dbCon))
 
-	e.Logger.Fatal(e.Start(":4001"))
-
+	e.Logger.Fatal(e.Start(":8081"))
 
 }
 
 func RegisterUser(c echo.Context) error {
 	param := new(User)
 	err := c.Bind(param)
-    if err != nil {
-    	log.Printf(err.Error())
-    	return c.JSON(http.StatusBadRequest , echo.Map{"status" : err.Error()})
+	if err != nil {
+		log.Printf(err.Error())
+		return c.JSON(http.StatusBadRequest, echo.Map{"status": err.Error()})
 	}
 
 	user := Users[param.Email]
 	log.Print("user", user)
- 	if user == nil {
-          newUser := &User{Email: param.Email}
-          Users[param.Email] = newUser
-	      return c.JSON(200, echo.Map{
-	      	"status" : "dang ki thanh cong",
-	      	"email"  : param.Email,
-		   })
+	if user == nil {
+		newUser := &User{Email: param.Email}
+		Users[param.Email] = newUser
+		return c.JSON(200, echo.Map{
+			"status": "dang ki thanh cong",
+			"email":  param.Email,
+		})
 	}
 
-	return c.JSON(http.StatusOK , echo.Map{
-		"status" : "user da ton tai",
-		"email" : param.Email,
+	return c.JSON(http.StatusOK, echo.Map{
+		"status": "user da ton tai",
+		"email":  param.Email,
 	})
 }
 
-func RegisterDevice(w http.ResponseWriter, r *http.Request)  {
+func RegisterDevice(w http.ResponseWriter, r *http.Request) {
 
-
-     //param := new(Device)
-     //if err := context.Bind(param) ; err!= nil {
-     //	return context.JSON(http.StatusBadRequest, echo.Map{"status": err.Error()})
-	 //}
-	 //user := Users[param.Email]
-	 //user.Tokens  = append(user.Tokens , param.Token)
-	 //
-	 //return context.JSON(200, echo.Map{
-	 //	"status" : "Your mobile has been registered",
-	 //	"email" : param.Email,
-	 //})
+	//param := new(Device)
+	//if err := context.Bind(param) ; err!= nil {
+	//	return context.JSON(http.StatusBadRequest, echo.Map{"status": err.Error()})
+	//}
+	//user := Users[param.Email]
+	//user.Tokens  = append(user.Tokens , param.Token)
+	//
+	//return context.JSON(200, echo.Map{
+	//	"status" : "Your mobile has been registered",
+	//	"email" : param.Email,
+	//})
 }
 
-func Push(c echo.Context) error  {
-	param := new(User)
-	err:= c.Bind(param)
-	if err != nil {
-		log.Print(err)
-	}
+func Push(dbGorm *gorm.DB) echo.HandlerFunc {
+	var arrTokenDevice = make([]string, 0)
 
-	//user  := Users[param.Email]
-    payload := Payload{
-		RegistrationIds: []string{"e-F294Izgqs:APA91bFGaCiRpfHog24IL7Vw1FmtoF4KYtFvYHIGJr0L6CfnMFns8NBZ9Zf8WgzLId_KmDgjhembvOdeo0-rlhklLqnFK1fi1YYMRTB4ywCwF-HJ7s1hmx9bJzsb6IkpqS4iogmgV_Or"},
-		Data:            Data{
-			Feature: "Hello Nam",
-			Body:    "Nice to meet u," ,
+	return func(c echo.Context) error {
+		//var hgUser HomegateUsers
+		homegateId := c.Request().Header.Get("homegate_id")
+		//res1 := dbGorm.Debug().First(&hgUser, "homegate_id = ?", hgIdGetRequest)
+		//if res1.Error != nil {
+		//	log.Print("error")
+		//}
+		//resValue := res1.Value
+		//b, err := json.Marshal(resValue)
+		//if err != nil {
+		//	log.Print(err.Error())
+		//}
+		//err = json.Unmarshal(b, &hgUser)
+
+		getTokenDev, err := dbGorm.Debug().
+			Table("users").
+			Select("users.token_devices").
+			Joins("left join homegate_users on users.id = homegate_users.user_id ").
+			Where("homegate_id = ?", homegateId).
+			Rows()
+		if err != nil {
+			log.Print(err.Error())
+		}
+		var tokenArr = make([]*TokenDevices, 0)
+
+		defer getTokenDev.Close()
+		for getTokenDev.Next() {
+			t := new(TokenDevices)
+			err = getTokenDev.Scan(&t.TokenDevice)
+			if err != nil {
+				log.Print(err)
+			}
+			tokenArr = append(tokenArr, t)
+		}
+		log.Print("tokenArr:::", tokenArr)
+		//var tokenDev TokenDevices
+
+		dbGorm.LogMode(true)
+		arrTokenDevice = append(arrTokenDevice, "fZQiZf6T7HQ:APA91bG_4fyMRNzGN6g3-rzfZEr77C47KySEeGFz_wGOkDW7NwkGbcjtT1h2nh88aXwqnd7TYykuIBtoV29MQtoz4b-PdI0Vu1eeweeB2M_9CIX9eszU72-IZv3v_9Bs9T_KPZ-VN1HG")
+
+		payload := Payload{
+			RegistrationIds: arrTokenDevice,
+			Data: Data{
+				Title: "Humidity sensor",
+				Body:  "a text",
+				Notification: messaging.Notification{
+					Title:    "sd",
+					Body:     "sd",
+					ImageURL: "https://img.lovepik.com/element/40035/8356.png_860.png",
+				},
+			},
+			Notification: Notification{
+				Title: "Humidity sensor",
+				Body:  "High temperature . Should I decrease the air conditional",
+				//Icon :
+			},
+		}
+		bPayload, _ := json.Marshal(payload)
+		go pushNotify(bPayload)
+		return c.JSON(http.StatusOK, "sd")
+	}
+}
+
+func Push1(c echo.Context) (err error) {
+	var arrTokenDevice = make([]string, 0)
+	arrTokenDevice = append(arrTokenDevice, "fZQiZf6T7HQ:APA91bG_4fyMRNzGN6g3-rzfZEr77C47KySEeGFz_wGOkDW7NwkGbcjtT1h2nh88aXwqnd7TYykuIBtoV29MQtoz4b-PdI0Vu1eeweeB2M_9CIX9eszU72-IZv3v_9Bs9T_KPZ-VN1HG")
+
+	payload := Payload{
+		RegistrationIds: arrTokenDevice,
+		Data: Data{
+			Title: "Humidity sensor",
+			Body:  "a text",
+			Notification: messaging.Notification{
+				Title:    "sd",
+				Body:     "sd",
+				ImageURL: "https://img.lovepik.com/element/40035/8356.png_860.png",
+			},
+		},
+		Notification: Notification{
+			Title: "Humidity sensor",
+			Body:  "High temperature . Should I decrease the air conditional",
+			//Icon :
 		},
 	}
-	bPayload , _  := json.Marshal(payload)
+	bPayload, _ := json.Marshal(payload)
 	go pushNotify(bPayload)
 
 	return c.JSON(http.StatusOK, echo.Map{"status": "Dang push"})
 }
 
 func pushNotify(payload []byte) {
-	  req,  _ := http.NewRequest("POST","https://fcm.googleapis.com/fcm/send", bytes.NewBuffer(payload))
+	req, _ := http.NewRequest("POST", "https://fcm.googleapis.com/fcm/send", bytes.NewBuffer(payload))
 
-	  req.Header.Set("Content-Type","application/json")
-	  req.Header.Set("Authorization","key=AAAAmlwBCtY:APA91bEn7bT8YgMCY9vR0iaHAJmonIA7_xownDfpur5GajXpahdtMzX6h0SWdKGg-eJmZYjQyV1H55Td1cyop6N7puYxHRDz0d27cCxaIH9oNDFNxbhYHrDl-Ab56Ak36olfKMuPI6dX")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "key=AAAANbra0SM:APA91bEoUGX77jMMl60L297UJYAKa8nKk1ImI5eTsz6FyFiTU7FhFNGQadKbwUUpDOKfZqUHGJ0oI2G-ane5jux3IEvQ4j2e6EJkWWwQvUeKQARd8o-RwHPnzVm0m4NvaZMkHbZOKIqP")
 
-	  cl := http.Client{}
-	  response , _ :=  cl.Do(req)
+	cl := http.Client{}
+	response, _ := cl.Do(req)
 
-	  b, _ := ioutil.ReadAll(response.Body)
-	  responseString := string(b)
-	  log.Print(responseString)
+	b, _ := ioutil.ReadAll(response.Body)
+	responseString := string(b)
+	log.Print(responseString)
 
-	  defer response.Body.Close()
+	// Response is a message ID string.
+
+	defer response.Body.Close()
 
 }
 
@@ -137,7 +248,7 @@ func sendToToken(app *firebase.App) {
 	}
 
 	// This registration token comes from the client FCM SDKs.
-	registrationToken := "eCtSd7Ymvo4:APA91bEGEbqbSvQKVUvw_xO-8aQV6OaT8xOCESVgyouaB4GWZSvUkX3718Ll4yKApqrA0ZYv-Inu91oB2nzPefe7YAoTpl6DL2PN_1JjXrZLccmQzI1dW_pTbqAU7stTDlI1V-WAh6gN"
+	registrationToken := "dCHGQ1nAdIc:APA91bFwBpS6bYtrX2gYTAD-atljc9jipasjg5XkN6z7CWPThc90mopFDA2tmH8kN_KnXqpRhMxAiIHao7GUaiK0MXDTtsxyT_JhbADSXTSbiETCakkwhW5pJOU5mo_wmHyJs_O_WK0q"
 
 	// See documentation on defining a message payload.
 	message := &messaging.Message{
